f46b03e9c09c7e8524936f94735d28ac196ea62e
fgeiger <florian.geiger21@gmail.com>
2025-04-01 09:08:51 +0200

    creating a card View

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,38 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Florian Geiger on 31.03.25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lenthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lenthInMinutes) minute meeting")
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+#Preview (traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    return CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Florian Geiger on 31.03.25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum{
+    var title: String
+    var attendees: [String]
+    var lenthInMinutes: Int
+    var theme: Theme
+}
